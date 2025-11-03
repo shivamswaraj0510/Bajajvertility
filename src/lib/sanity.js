2025e2a2da99fee6b38f3685d0940c41cfc067a9
Shivam Kumar80 <shivamkumar80@L-FVFG5BZTQ05D.local>
2025-11-03 15:44:17 +0530

    package updated

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,10 +1,10 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "6lbqme4u", // ✅ your project ID
-  dataset: "production", // ✅ your dataset name
-  apiVersion: "2025-01-01", // or the version you're using
-  useCdn: false, // ❌ turn this off for private datasets
+  projectId: "6lbqme4u", 
+  dataset: "production", 
+  apiVersion: "2025-01-01", 
+  useCdn: false,
   token:
     "skbpqOdqhEtsQ1fdnNnSSuxvG5o4K1ylgdnqI5GCMzu8kIE76EbvmygHjgsoaNFKEB9wqwEid92GbBpHpSGgMvVDotY8mteSsUQ5jfTqteCxHNdKjzOYOD2YFv9o1jmV9dUxkEzQdFBI1VpvB4590VjqPlRTioVjwghwgJni7TV85FQdJ25G", // 🔹 Add this line (see below)
 });
